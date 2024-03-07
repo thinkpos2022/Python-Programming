34726b337e5cf0312ad02a5d773fb73b070bef9d
nthanonklin <nthanonklin@gmail.com>
2024-03-06 16:43:35 -0800

    add containDuplicates

---
+++ containsDuplicate.py
@@ -0,0 +1,41 @@
+"""Given an integer array nums, return true if any value appears at least twice in the array, 
+and return false if every element is distinct.
+
+Test case 1:
+Input: nums = [1,2,3,1]
+Output: true
+
+Test case 2:
+Input: nums = [1,2,3,4]
+Output: false
+
+Test case 3:
+Input: nums = [1,1,1,3,3,4,3,2,4,2]
+Output: true
+"""
+
+class ContainsDuplicate:
+    def check_duplicate(self, nums):
+        unique_set = set()
+        for num in nums:
+            if num in unique_set:
+                return True
+            unique_set.add(num)
+        return False
+       
+
+
+nums1 = [1, 2, 3, 1]
+duplicate_checker = ContainsDuplicate()
+output1 = duplicate_checker.check_duplicate(nums1)
+print(output1)
+
+nums2 = [1, 2, 3, 4]
+duplicate_checker = ContainsDuplicate()
+output2 = duplicate_checker.check_duplicate(nums2)
+print(output2)
+
+nums3 = [1, 1, 1, 3, 3, 4, 3, 2, 4, 2]
+duplicate_checker = ContainsDuplicate()
+output3 = duplicate_checker.check_duplicate(nums3)
+print(output3)
